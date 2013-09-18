00664f5be4f8ee95ead375104611728fc960742e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-09-18 11:34:04 +0200

    deprecate RoamingTypeExtensionPoint

--- src/com/intellij/vssSupport/Configuration/VssConfiguration.java
+++ src/com/intellij/vssSupport/Configuration/VssConfiguration.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 
 @State(
   name="VssConfiguration",
+  roamingType = RoamingType.DISABLED,
   storages= {
     @Storage(
       file = StoragePathMacros.WORKSPACE_FILE
