36ccf3ea4d22bb4e41dd0118c018264ce36ce2b8
Alexey Kudravtsev <cdr@intellij.com>
2013-12-24 19:26:05 +0400

    notnull

--- src/com/intellij/vssSupport/VssBundle.java
+++ src/com/intellij/vssSupport/VssBundle.java
@@ -5,6 +5,7 @@ package com.intellij.vssSupport;
 
 import com.intellij.CommonBundle;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
 import java.lang.ref.Reference;
@@ -12,15 +13,15 @@ import java.lang.ref.SoftReference;
 import java.util.ResourceBundle;
 
 public class VssBundle {
-  private static Reference<ResourceBundle> ourBundle;
 
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return CommonBundle.message(getBundle(), key, params);
+  }
+
+  private static Reference<ResourceBundle> ourBundle;
   @NonNls private static final String BUNDLE = "com.intellij.vssSupport.VssBundle";
 
   private VssBundle() {
-  }
-
-  public static String message(@PropertyKey(resourceBundle = BUNDLE)String key, Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
   }
 
   private static ResourceBundle getBundle() {
